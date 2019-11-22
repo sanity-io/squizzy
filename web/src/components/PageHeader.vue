36871302ef5b2f0ffb3844e92801f793a4b2b2b8
vicbergquist <victoria.bergquist@gmail.com>
2019-11-22 15:14:17 +0100

    [web] add match title to match screen

--- web/src/components/PageHeader.vue
+++ web/src/components/PageHeader.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="squizzy-page">
     <div class="header">
+      <h1 v-if="match" class="match-title">{{ match }}</h1>
       <squizzy-squid />
-      <h1 class="heading">{{ title }}</h1>
+      <h2 class="heading">{{ title }}</h2>
       <p v-if="message" class="message">{{ message }}</p>
     </div>
     <slot />
@@ -16,6 +17,10 @@ export default {
     SquizzySquid
   },
   props: {
+    match: {
+      type: [String, Boolean],
+      default: false
+    },
     title: {
       type: String,
       required: true
@@ -31,6 +36,13 @@ export default {
 <style lang="sass" scoped>
 .squizzy-page
   text-align: center
+
+.match-title
+  margin: 0.5rem 0
+  font-size: 1rem
+  font-weight: normal
+  text-transform: uppercase
+  letter-spacing: 1px
 
 .heading
   font-size: 2rem
