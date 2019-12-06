53cd94535471f54b2021edcaf6631ce7d590baab
vicbergquist <victoria.bergquist@gmail.com>
2019-12-06 13:56:14 +0100

    [web] fix wrong folder name

--- web/src/components/quiz/question/ChoiceCard.vue
+++ web/src/components/quiz/question/ChoiceCard.vue
@@ -38,7 +38,7 @@ export default {
   computed: {
     icon() {
       const icons = ['Circle', 'Star', 'Triangle', 'Square']
-      return () => import(`../icons/${icons[this.index]}Icon.vue`)
+      return () => import(`./symbols/${icons[this.index]}Icon.vue`)
     }
   }
 }
