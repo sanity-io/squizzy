61d01556131abcc6ff8b5ecbecb3460c2b2ff577
vicbergquist <victoria.bergquist@gmail.com>
2019-12-11 14:53:47 +0100

    [web] prettify leave button

--- web/src/components/TheNavbar.vue
+++ web/src/components/TheNavbar.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="navbar">
-    <router-link to="/">{{ $appName }}</router-link>
-    <button v-if="showLeaveButton" class="btn" @click="leaveGame">Leave game</button>
+    <div class="app-name">{{ $appName }}</div>
+    <button v-if="showLeaveButton" class="leave-button" @click="leaveGame">
+      <span class="leave-label">Leave</span>
+      <span class="leave-icon" role="img">
+        <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M215.469 332.802l29.863 29.864L352 256 245.332 149.333l-29.863 29.865 55.469 55.469H64v42.666h205.864l-54.395 55.469zM405.334 64H106.666C83.198 64 64 83.198 64 106.666V192h42.666v-85.333h298.668v298.668H106.666V320H64v85.334C64 428.802 83.198 448 106.666 448h298.668C428.802 448 448 428.802 448 405.334V106.666C448 83.198 428.802 64 405.334 64z"
+          />
+        </svg>
+      </span>
+    </button>
   </div>
 </template>
 
 <script>
-// TODO: prettify leave button
 export default {
   computed: {
     showLeaveButton() {
-      const player = this.$store.state.player
+      const hasPlayer = this.$store.state.playerStore.player
       const hasMatch = this.$store.state.matchStore.match
-      const hasPlayer = player && player.player
       return hasMatch || hasPlayer
     }
   },
@@ -32,9 +39,22 @@ export default {
   justify-content: space-between
   align-items: center
   min-height: $navbar-height
-  font-size: $font-size-medium
+  padding: 0.5rem
   font-weight: 700
-  text-transform: uppercase
-  padding: 0.5rem 1rem
-  letter-spacing: 2px
+  font-size: $font-size-large
+  // text-transform: uppercase
+
+.leave-button
+  font-size: $font-size-small
+  letter-spacing: 1px
+  display: flex
+  align-items: center
+
+  .leave-label
+    padding-right: 0.25rem
+
+  .leave-icon svg
+    display: flex
+    height: 1.5em
+    fill: $color-white
 </style>
