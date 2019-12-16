9c65a936613c397ed94ba353026da878a35035a8
Thomas Drevon <thomas@sanity.io>
2019-12-16 13:39:59 +0100

    Add fieldset to match, containing all the state data

--- studio/schemas/documents/match.js
+++ studio/schemas/documents/match.js
@@ -1,114 +1,119 @@
-import UUID from "@sanity/uuid";
-import { MdTv } from "react-icons/md";
+import UUID from '@sanity/uuid'
+import {MdTv} from 'react-icons/md'
 
-const createSlug = () => UUID().substring(0, 5);
+const createSlug = () => UUID().substring(0, 5)
 
 export default {
-  name: "match",
-  title: "Match",
-  type: "document",
+  name: 'match',
+  title: 'Match',
+  type: 'document',
   icon: MdTv,
-  description: "An instance of a Quiz",
+  description: 'An instance of a Quiz',
+  fieldsets: [
+    {
+      name: 'match-state',
+      title: 'Danger Zone - match state data',
+      options: {
+        collapsible: true,
+        collapsed: false
+      }
+    }
+  ],
   fields: [
     {
-      name: "slug",
-      title: "Slug",
-      type: "slug",
-      description:
-        "Generates a QR-code. The game can also be accessed on /match/the-slug.",
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      description: 'Generates a QR-code. The game can also be accessed on /match/the-slug.',
       options: {
         slugify: createSlug
       }
     },
     {
-      name: "quiz",
-      title: "Quiz",
-      type: "reference",
+      name: 'quiz',
+      title: 'Quiz',
+      type: 'reference',
+      description: 'You need to have published your quiz before it appears here.',
+      to: [{type: 'quiz'}]
+    },
+    {
+      name: 'startedAt',
+      title: 'Started At',
+      type: 'datetime',
+      description: 'This field is managed by a serverless function.',
+      fieldset: 'match-state'
+    },
+    {
+      name: 'finishedAt',
+      title: 'Finished At',
+      type: 'datetime',
+      description: 'This field is managed by a serverless function.',
+      fieldset: 'match-state'
+    },
+    {
+      name: 'currentQuestionKey',
+      title: 'Current Question Key',
+      type: 'string',
       description:
-        "You need to have published your quiz before it appears here.",
-      to: [{ type: "quiz" }]
+        'String referring to a question by _key (in quiz.questions[]). This field is managed by a serverless function.',
+      fieldset: 'match-state'
     },
     {
-      name: "startedAt",
-      title: "Started At",
-      type: "datetime",
-      description: "This field is managed by a serverless function.",
-      readOnly: true
-    },
-    {
-      name: "finishedAt",
-      title: "Finished At",
-      type: "datetime",
-      description: "This field is managed by a serverless function.",
-      readOnly: true
-    },
-    {
-      name: "currentQuestionKey",
-      title: "Current Question Key",
-      readOnly: true,
-      type: "string",
+      name: 'isCurrentQuestionOpen',
+      title: 'Current Question Open',
+      type: 'boolean',
       description:
-        "String referring to a question by _key (in quiz.questions[]). This field is managed by a serverless function."
+        'Is the current question open to receive answers? This field is managed by a serverless function.',
+      fieldset: 'match-state'
     },
     {
-      name: "isCurrentQuestionOpen",
-      title: "Current Question Open",
-      readOnly: true,
-      type: "boolean",
+      name: 'players',
+      title: 'Players',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'player'}}],
       description:
-        "Is the current question open to receive answers? This field is managed by a serverless function."
+        'Players who have joined the match. This field is managed by a serverless function',
+      fieldset: 'match-state'
     },
     {
-      name: "players",
-      title: "Players",
-      readOnly: true,
-      type: "array",
-      of: [{ type: "reference", to: { type: "player" } }],
+      name: 'answers',
+      title: 'Answers',
+      type: 'array',
+      of: [{type: 'answer'}],
       description:
-        "Players who have joined the match. This field is managed by a serverless function"
-    },
-    {
-      name: "answers",
-      title: "Answers",
-      description:
-        "The answers given by the players. This field is managed by a serverless function",
-      readOnly: true,
-      type: "array",
-      of: [{ type: "answer" }]
+        'The answers given by the players. This field is managed by a serverless function',
+      fieldset: 'match-state'
     }
   ],
   initialValue: () => ({
     slug: {
       current: createSlug(),
-      _type: "slug"
+      _type: 'slug'
     }
   }),
   preview: {
     select: {
-      title: "quiz.title",
-      slug: "slug.current",
-      players: "players",
-      startedAt: "startedAt",
-      finishedAt: "finishedAt"
+      title: 'quiz.title',
+      slug: 'slug.current',
+      players: 'players',
+      startedAt: 'startedAt',
+      finishedAt: 'finishedAt'
     },
-    prepare({ title, slug, players, startedAt, finishedAt }) {
-      const isOngoing = startedAt && !finishedAt;
-      const isNotYetStarted = !startedAt && !finishedAt;
-      const numberOfPlayers =
-        players && players.length > 0 ? players.length : 0;
-      let subtitle;
+    prepare({title, slug, players, startedAt, finishedAt}) {
+      const isOngoing = startedAt && !finishedAt
+      const isNotYetStarted = !startedAt && !finishedAt
+      const numberOfPlayers = players && players.length > 0 ? players.length : 0
+      let subtitle
       if (isNotYetStarted) {
-        subtitle = "Not yet started";
+        subtitle = 'Not yet started'
       } else {
-        subtitle = isOngoing ? "Is ongoing!" : "Finished";
+        subtitle = isOngoing ? 'Is ongoing!' : 'Finished'
       }
-      subtitle = `${subtitle} - ${numberOfPlayers} player${
-        numberOfPlayers === 1 ? "" : "s"
-      }`;
+      subtitle = `${subtitle} - ${numberOfPlayers} player${numberOfPlayers === 1 ? '' : 's'}`
       return {
-        title: `[${slug || "missing slug"}] ${title || "untitled"}`,
+        title: `[${slug || 'missing slug'}] ${title || 'untitled'}`,
         subtitle
-      };
+      }
     }
   }
-};
+}
