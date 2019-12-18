6625455aadc159c48b01a8efd7ea7f5341ea8a84
Knut Melvær <knut.melvaer@gmail.com>
2019-12-18 18:45:01 +0100

    add more helpful comments

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,6 +1,9 @@
 const sanityClient = require("@sanity/client");
+
 export default sanityClient({
+  // replace this id with the one you find in /studio/sanity.json
   projectId: "puj7p168",
+  // make sure that the dataset name is the same as the one in /studio/sanity.json
   dataset: "production",
   useCdn: false
 });
